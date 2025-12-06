8f4aa4fd2cd5a6e83ceb1963f87766d5c9ed68aa
amanullah7649 <mdamanullah7649@gmail.com>
2025-12-07 01:10:36 +0600

    added a get user decorator

---
+++ src/common/decorators/getUser.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { IUser } from 'src/interfaces/user.interface';
+
+export const GetUser = createParamDecorator((data, ctx: ExecutionContext): IUser => {
+    const req = ctx.switchToHttp().getRequest();
+    // const user = req.user
+    return req.user;
+});
